f20d91a425eb4c6ef0fed2059e01ef1d725ebec4
snehamaddhichettyant <snea913@gmail.com>
2024-04-03 19:09:54 -0400

    Assert Test for Default value

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,16 +4,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+    @LocalServerPort
+    private int port;
 
     @Autowired
+    private TestRestTemplate restTemplate;
     private DoubleNumController controller;
 
     @Test
     void contextLoads() throws Exception {
         assertThat(controller).isNotNull();
     }
+    @Test
+    void doubleShouldReturnDefaultValue() {
+
+        int result = this.restTemplate.getForObject("http://localhost:" + port + "/double?value=" + 5, Integer.class);
+        assertThat(result).isEqualTo(10);
+    }
+
 }
 
