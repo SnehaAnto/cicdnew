602dae7773c36294f3adae3d9ac0290ee13f892f
snehamaddhichettyant <snea913@gmail.com>
2024-04-03 18:59:18 -0400

    Controller assertion

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,19 @@
 package com.example.demo;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private DoubleNumController controller;
 
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
 }
 
